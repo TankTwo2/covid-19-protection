13099761873c2df0af99e7d81717079405bb543f
Tanktwo <rhkdtjr900@gmail.com>
2020-09-13 23:11:50 +0900

    bulma structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
+const styles: { [index: string]: any } = {
+  textAlign: 'center',
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">123</header>
+      <div className="container">
+        <br />
+        <div className="columns">
+          <div style={styles} className={'column is-one-third'}>
+            1
+          </div>
+          <div style={styles} className={'column is-one-third'}>
+            2
+          </div>
+          <div style={styles} className={'column is-one-third'}>
+            3
+          </div>
+        </div>
+        <div className="columns">
+          <div style={styles} className={'column is-one-third'}>
+            4
+          </div>
+          <div style={styles} className={'column is-one-third'}>
+            5
+          </div>
+          <div style={styles} className={'column is-one-third'}>
+            6
+          </div>
+        </div>
+        <div className="columns">
+          <div style={styles} className={'column is-one-third'}>
+            7
+          </div>
+          <div style={styles} className={'column is-one-third'}>
+            8
+          </div>
+          <div style={styles} className={'column is-one-third'}>
+            9
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
